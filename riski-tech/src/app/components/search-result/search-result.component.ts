eaac664a44459d7103c77e042599c716ccf7cd16
kobi bitaolin <amk999@gmail.com>
2020-03-04 16:24:39 +0200

    display search result

--- riski-tech/src/app/components/search-result/search-result.component.ts
+++ riski-tech/src/app/components/search-result/search-result.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-search-result',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultComponent implements OnInit {
 
+  @Input() movie;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.movie);
   }
+
+ 
+
 
 }
--- riski-tech/src/app/components/search/search.component.ts
+++ riski-tech/src/app/components/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TheMovieDBService } from 'src/app/services/the-movie-db.service';
-import {Movie} from '../../models/movie';
+import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'app-search',
@@ -12,22 +12,26 @@ export class SearchComponent implements OnInit {
 
   constructor(private theMovieDB: TheMovieDBService) { }
 
-  movies:Movie[]=[];
+  movies: Movie[] = [];
 
   ngOnInit(): void {
 
   }
 
   handleSubmit(moviesForm) {
-    console.log("submit");
+    console.log('submit');
 
     const query: string = moviesForm.value.query;
 
     this.theMovieDB.getMovies(query)
       .subscribe(data => {
+        data.map(movie =>
+          movie.poster_path = 'https://image.tmdb.org/t/p/w500' + movie.poster_path)
         this.movies = data;
         console.log(data);
-      })
+      });
   }
+
+ 
 
 }
