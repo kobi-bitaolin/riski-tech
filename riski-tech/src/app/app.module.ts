5d886bdc1a19e9c6c847f1f83b023f1e249eaa04
kobi bitaolin <amk999@gmail.com>
2020-03-04 11:00:36 +0200

    components

--- riski-tech/src/app/app.module.ts
+++ riski-tech/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { HistoryComponent } from './components/history/history.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    HistoryComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule
