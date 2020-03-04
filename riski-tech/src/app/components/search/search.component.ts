3bc781a73e285c9195c0d2a2cb0e29748ae84f40
Avshalom <you@example.com>
2020-03-04 14:50:57 +0200

    bug fix

--- riski-tech/src/app/components/search/search.component.ts
+++ riski-tech/src/app/components/search/search.component.ts
@@ -1,31 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { TheMovieDBService } from 'src/app/services/the-movie-db.service';
+import {Movie} from '../../models/movie';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
 
   constructor(private theMovieDB: TheMovieDBService) { }
 
-  movies;
+  movies:Movie[]=[];
 
   ngOnInit(): void {
-    console.log();
 
   }
 
-  handleSubmit(movies) {
+  handleSubmit(moviesForm) {
     console.log("submit");
 
-    const query: string = movies.value.query;
+    const query: string = moviesForm.value.query;
 
     this.theMovieDB.getMovies(query)
       .subscribe(data => {
         this.movies = data;
-        console.log(this.movies);
+        console.log(data);
       })
   }
 
--- riski-tech/src/app/services/the-movie-db.service.ts
+++ riski-tech/src/app/services/the-movie-db.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-
+import { Observable } from 'rxjs';
+import {Movie} from '../models/movie';
+import {map} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +15,9 @@ apiKey:string =  '3ea150464035bc92e08e1ecfd93b9557';
 
   constructor(private http: HttpClient) { }
 
-  getMovies(query: string) {
+  getMovies(query: string):Observable<Movie[]> {
     let url = `${this.baseUrl}api_key=${this.apiKey}&language=enUS&query=${query}&page=1&include_adult=false`
-    return this.http.get(url)
-  }
+    return this.http.get<any>(url)
+    .pipe(map(res=> res.results))}
+
 }
